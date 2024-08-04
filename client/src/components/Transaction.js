48d22362b4999635c02908459b22be70d0e2a474
Mohit Agarwal <mohitagarwal.1031@gmail.com>
2024-08-04 09:23:55 +0530

    Integrate GlobalContext into remaining components; add delete feature

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+    const sign = transaction.amount < 0 ? '-' : '+';
     return (
-        <li className="minus">
-            {transaction.text}<span>Rs. 400</span><button className="delete-btn">x</button>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text}<span>{sign}Rs. {Math.abs(transaction.amount)}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
         </li>
     )
 }
