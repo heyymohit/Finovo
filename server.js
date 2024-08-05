a585e79cd40d642d5c7bde88b84061f111ea772f
Mohit Agarwal <mohitagarwal.1031@gmail.com>
2024-08-05 06:00:11 +0530

    Build project for production

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/transactions', transactions);
 
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'));    //
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));  //__dirname -> current dir, go to client -> build -> index.html
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
